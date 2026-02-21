981a1d288b0b655bcbf385aa57a092a0c8b516d3
idkitsnotme <bazick8@gmail.com>
2026-02-20 17:01:20 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, getVoiceConnection } = require('@discordjs/voice');
-const ffmpeg = require('ffmpeg-static'); // Add this line
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus, getVoiceConnection } = require('@discordjs/voice');
 
 const client = new Client({
     intents: [
@@ -11,31 +10,35 @@ const client = new Client({
     ]
 });
 
+client.on('ready', () => {
+    console.log(`Bot logged in as ${client.user.tag}`);
+});
+
 client.on('messageCreate', async (message) => {
     if (message.author.bot || !message.content) return;
 
     const voiceChannel = message.member.voice.channel;
+    
     if (voiceChannel) {
         try {
             const connection = joinVoiceChannel({
                 channelId: voiceChannel.id,
                 guildId: message.guild.id,
                 adapterCreator: message.guild.voiceAdapterCreator,
+                selfDeaf: false // Important so bot doesn't look deafened
             });
 
+            // Google TTS Link
             const url = `https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(message.content)}&tl=en&client=tw-ob`;
             
-            // We tell the resource exactly where FFmpeg is
-            const resource = createAudioResource(url, {
-                inlineVolume: true
-            });
-            
+            const resource = createAudioResource(url);
             const player = createAudioPlayer();
+
             player.play(resource);
             connection.subscribe(player);
 
             player.on('error', error => {
-                console.error('Player Error:', error.message);
+                console.error('Audio Player Error:', error.message);
             });
 
         } catch (error) {
